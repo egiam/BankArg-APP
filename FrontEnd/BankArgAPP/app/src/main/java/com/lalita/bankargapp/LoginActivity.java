7977d89f30a76a4a5678598f3d9fd51da19a7789
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2023-09-24 14:49:47 -0700

    Update de Login y primeros pasos de Perfil

--- FrontEnd/BankArgAPP/app/src/main/java/com/lalita/bankargapp/LoginActivity.java
+++ FrontEnd/BankArgAPP/app/src/main/java/com/lalita/bankargapp/LoginActivity.java
@@ -3,12 +3,38 @@ package com.lalita.bankargapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, PerfilActivity.class);
+                    startActivities(new Intent[]{intent});
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
